552f5b6fb7361f89ada23f56cbcf2ef69c38ec62
ArcLKN <kalidolkn@gmail.com>
2024-05-08 20:02:56 +0200

    Event Management Page

---
+++ client/src/pages/EventManagementPage.js
@@ -0,0 +1,14 @@
+// EventManagementPage.js
+
+import React from 'react';
+
+function EventManagementPage() {
+  return (
+    <div>
+      <h1>Event Management Page</h1>
+      {/* Add more content here */}
+    </div>
+  );
+}
+
+export default EventManagementPage;
